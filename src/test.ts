c1432131b9f713855fd79426993b806436a3bda4
James Njuguna <jameskinyua590@gmail.com>
2021-08-27 14:35:14 +0300

    configure eslint & prettier

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,15 @@
-function foo(bar: string) {
-	return `Hello, ${bar}`;
-}
+let foo: string;
+let bar: boolean;
+let baz: number;
+let qux: string[];
+let quuz: [number, string];
+let coorge: { [key: number]: string };
+let grault: Set<number>;
 
-let baz = 'ABC';
-
-console.log(foo(baz));
+foo = 'ABC';
+bar = true;
+baz = 123;
+qux = ['a', 'b', 'c'];
+quuz = [1, 'abc'];
+coorge = { 123: 'abc', 465: 'def' };
+grault = new Set([1, 2, 3]);
