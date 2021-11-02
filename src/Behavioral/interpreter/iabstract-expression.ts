ed904568337e4a069fd3172e47403bfbe9187c24
James Njuguna <jameskinyua590@gmail.com>
2021-11-02 18:12:42 +0300

    Implement Interpreter design pattern

---
+++ src/Behavioral/interpreter/iabstract-expression.ts
@@ -0,0 +1,10 @@
+export default interface IAbstractExpression {
+  /**
+   * All Terminal and Non-Terminal expressions will implement
+   * an `interpret` method
+   */
+  value?: number;
+  left?: IAbstractExpression;
+  right?: IAbstractExpression;
+  interpret(): number;
+}
